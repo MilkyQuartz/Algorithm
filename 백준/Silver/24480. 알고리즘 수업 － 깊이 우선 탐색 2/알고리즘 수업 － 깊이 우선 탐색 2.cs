aa2ffee3fca0e6bf4f742345c1e109df757936c5
MilkyQuartz <141620531+MilkyQuartz@users.noreply.github.com>
2024-04-27 21:18:52 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 328 ms, Memory: 31220 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cs
@@ -0,0 +1,82 @@
+using System;
+
+namespace Baekjoon
+{
+
+    class Program
+    {
+        static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+        static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+        static int N, M, R;
+        static List<int>[] graph;
+        static int[] order;
+        static bool[] visited;
+        static int visitorder = 1;
+
+        static void Main()
+        {
+            string[] inputs = sr.ReadLine().Split(" ");
+            N = int.Parse(inputs[0]);
+            M = int.Parse(inputs[1]);
+            R = int.Parse(inputs[2]);
+
+            graph = new List<int>[N + 1]; 
+            for (int i = 0; i <= N; ++i)
+            {
+                graph[i] = new List<int>();
+            }
+
+            for (int i = 0; i < M; ++i)
+            {
+                inputs = sr.ReadLine().Split(" ");
+
+                int u = int.Parse(inputs[0]);
+                int v = int.Parse(inputs[1]);
+                graph[u].Add(v);
+                graph[v].Add(u);
+            }
+
+            for (int i = 1; i <= N; ++i)
+            {
+                graph[i].Sort();
+                graph[i].Reverse();
+            }
+
+            order = new int[N + 1];
+            visited = new bool[N + 1];
+            dfs(R);
+
+            for (int i = 1; i <= N; ++i)
+            {
+                sw.WriteLine(order[i]);
+            }
+
+            sr.Close();
+            sw.Close();
+
+        }
+
+        static void dfs(int node)
+        {
+            if (visited[node])
+            {
+                return;
+            }
+
+            visited[node] = true;
+
+            order[node] = visitorder;
+            ++visitorder;
+
+            foreach (int next in graph[node])
+            {
+                if (visited[next] == false)
+                {
+                    dfs(next);
+                }
+            }
+        }
+
+    }
+}
