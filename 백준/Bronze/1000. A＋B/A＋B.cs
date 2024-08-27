aa1c689a68b6c63b33020df07a3bbd8f82857d21
MilkyQuartz <141620531+MilkyQuartz@users.noreply.github.com>
2024-08-27 22:59:44 +0900

    [Bronze V] Title: A+B, Time: 52 ms, Memory: 5488 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cs
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -1,15 +1,2 @@
-using System;
-using static System.Console;
-
-namespace TestWorld
-{
-    internal class MainApp
-    {
-        static void Main(string[] args)
-        {
-            string[] s = Console.ReadLine().Split();
-
-            Console.WriteLine(int.Parse(s[0]) + int.Parse(s[1]));
-        }
-    }
-}
+string[] input = Console.ReadLine().Split(' ');
+Console.Write(int.Parse(input[0]) + int.Parse(input[1]));
