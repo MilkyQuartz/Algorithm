0547493e7f834df143d551deb25f73e3b540cbaf
MilkyQuartz <141620531+MilkyQuartz@users.noreply.github.com>
2025-03-19 14:41:14 +0900

    [Bronze V] Title: A+B, Time: 48 ms, Memory: 5504 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.cs
+++ 백준/Bronze/1000. A＋B/A＋B.cs
@@ -1,2 +1,6 @@
-string[] input = Console.ReadLine().Split();
-Console.Write(int.Parse(input[0]) + int.Parse(input[1]));
+string[] s = Console.ReadLine().Split();
+int n = int.Parse(s[0]);
+int m = int.Parse(s[1]);
+int result = n + m;
+
+Console.WriteLine(result);
